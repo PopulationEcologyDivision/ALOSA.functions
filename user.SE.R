6bd40f27e6925732f8569af139587fdb0ddc9149
S-Fulton <103256532+S-Fulton@users.noreply.github.com>
2022-06-23 11:17:04 -0300

    Delete user.SE.R

--- user.SE.R
+++
@@ -1,9 +0,0 @@
-#
-# User defined standard error
-#
-# This function is used by:
-#     - onespecies.river.escapement
-# This functions uses:
-#     - no other user defined functions used.
-
-user.SE=function(x){sd(x,na.rm=T)/sqrt(sum(!is.na(x)))}
