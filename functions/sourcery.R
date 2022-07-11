313be007c053958be32e4a8b975be16a517973ba
Fulton <Samantha.Fulton@dfo-mpo.gc.ca>
2022-07-11 13:53:18 -0300

    No longer can you select WD. It will now use ~/git/ALOSA.functions/functions

--- functions/sourcery.R
+++ functions/sourcery.R
@@ -1,6 +1,6 @@
 
 sourcery<-function(){
-  setwd(choose.dir(caption = "Navigate to folder where functions are located"))
+  setwd("~/git/ALOSA.functions/functions")
   filenames=list.files(path = getwd(), full.names = T)
   for (i in 1:length(filenames)){
     source(filenames[i])
