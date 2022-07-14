98be64bbb6f45afa1e89e90b72fa20a34613c7b6
Fulton <Samantha.Fulton@dfo-mpo.gc.ca>
2022-07-14 14:32:04 -0300

    Added comments

--- functions/sourcery.R
+++ functions/sourcery.R
@@ -1,5 +1,6 @@
 
 sourcery<-function(){
+  #Soure all the files in the git/ALOSA.functions directory
   setwd("~/git/ALOSA.functions/functions")
   filenames=list.files(path = getwd(), full.names = T)
   for (i in 1:length(filenames)){
