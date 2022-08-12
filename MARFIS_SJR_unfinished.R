bed3f1485f98382c1d9f64f594485dd2e2963c5f
Billard <Mark.Billard@dfo-mpo.gc.ca>
2022-08-12 16:16:02 -0300

    added some code to correct and pull out gill net catch for 17-19

--- MARFIS_SJR_unfinished.R
+++ MARFIS_SJR_unfinished.R
@@ -9,9 +9,12 @@ library(dplyr)
 catch.sj<-catch[catch$RIVERNAME_CLEANED=="SAINT JOHN",]
 catch.sj<-catch.sj[complete.cases(catch.sj[,'LICENCE_ID']),]
 
+##Data fix before conversion
+catch.sj$MEASUREMENT_UNIT[catch.sj$LICENCE_ID==120285 | catch.sj$YEAR==2017]<-"POUNDS"
+
 catch.sj<-convert.KGS(catch.sj)
 
-gill<-catch.sj[catch.sj$FV_GEAR_CODE==41,]
+gill<-catch.sj[catch.sj$FV_GEAR_CODE==41 | catch.sj$FV_GEAR_CODE==42,]
 trap<-catch.sj[catch.sj$FV_GEAR_CODE==62,]
 
 table(trap$LICENCE_ID,trap$YEAR)
@@ -19,13 +22,12 @@ table(gill$LICENCE_ID,gill$YEAR)
 
 
 # gill.ind<-gill[gill$LICENCE_ID==120121,]
-trap.ind<-trap[trap$LICENCE_ID==120760,]
-
+# trap.ind<-trap[trap$LICENCE_ID==120760,]
 # gill.121<-aggregate(gill.ind$KGS,by=list(YEAR=gill.ind$YEAR),FUN="sum")
 # trap.121<-aggregate(trap.ind$KGS,by=list(YEAR=trap.ind$YEAR),FUN="sum")
 
-plot(trap.121$YEAR,trap.121$x)
-lines(gill.121$YEAR,gill.121$x)
+# plot(trap.121$YEAR,trap.121$x)
+# lines(gill.121$YEAR,gill.121$x)
 
 gill.sum<-aggregate(gill$KGS,by=list(gill$LICENCE_ID,gill$YEAR),FUN="sum")
 trap.sum<-aggregate(trap$KGS,by=list(trap$LICENCE_ID,trap$YEAR),FUN="sum")
@@ -50,7 +52,7 @@ df<-df[df$Year>2008 & df$Year<2022,]
 fit<-lm(Catch~Gear+Year,data=df)
 summary(fit)
 
-
+##plot
 boxplot(Catch~Gear+Year,data=df,col=c("red","blue"),
         names=c("2009","","2010","","2011","","2012","","2013","","2014","",
                 "2015","","2016","","2017","","2018","","2019","","2020","","2021",""
@@ -63,3 +65,29 @@ mean(df$Catch[df$Gear=="trap"])
 mean(df$Catch[df$Gear=="gill"])
 
 sub<-df[df$Year==2019 & df$Gear=="gill",]
+
+
+####Only Gill net catch####
+gill.sub<-gill[gill$YEAR==2017 | gill$YEAR==2018 | gill$YEAR==2019,]
+gill.sub<-gill.sub[complete.cases(gill.sub[,'LICENCE_ID']),]
+
+# gill.sub$KGS[gill.sub$LICENCE_ID==120121]<-gill.sub$KGS[gill.sub$LICENCE_ID==120121]/2.2
+
+gill.sub.sum<-aggregate(gill.sub$KGS,by=list(gill.sub$LICENCE_ID,gill.sub$YEAR),FUN="sum")
+
+##potential metrics for "typical" catch
+mean(gill.sub.sum$x)
+median(gill.sub.sum$x)
+
+mean(gill.sub.sum$x[gill.sub.sum$x>500])
+median(gill.sub.sum$x[gill.sub.sum$x>500])
+
+x<-tail(sort(gill.sub.sum$x),5)
+mean(x)
+
+###indv licences
+##2017 120121 - Number vs weight in kg issues corrected? Looks fixed
+##2017 120288 - catch high, logbook listed as kg but in MARFIS as pounds (if this is an error, catch would be even higher!)
+##2019 120274 - set 180 fa of gear code 41 AND 42 - double a typical licence????
+##2017 120052 - no apparent errors, 150 fathoms of net
+##2017 120285 - Wrong unit for most entries, log book indicates pounds. CORRECTED ABOVE
