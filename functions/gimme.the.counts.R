f9645d2f7593abd333eee55dbdc1c2ee30d83228
Nau <George.Nau@dfo-mpo.gc.ca>
2022-11-07 15:20:41 -0400

    fixed it

--- functions/gimme.the.counts.R
+++ functions/gimme.the.counts.R
@@ -2,14 +2,19 @@
 # run through onespecies.river.escapement.R for all years and paste together daily.summary to new large dataframe with same format as old excel sheet.
 gimme.the.counts <- function(years,
                              site){
-  all.counts <- NULL
-  for(i in length(years)){
+  
+  daily.summary <- list()
+  for(i in 1:length(years)){
     
-    daily.summary <- onespecies.river.escapement(year = years[i], site = site, channel = channel)
-    all.counts <- rbind(all.counts, daily.summary)
+    test <- onespecies.river.escapement(year = years[i], site = site, channel = channel)
+    test$year <- years[i]
+    
+    daily.summary[[i]] <- test
+    #all.counts <- rbind(all.counts, daily.summary)
     
   }
+  all.counts <- do.call("rbind", daily.summary)
   return(all.counts)
 }
   
-test <- gimme.the.counts(years = c(2013, 2022), site = 2)
+out <- gimme.the.counts(years = c(2015:2019), site = 3)
