e961becd34c5b3632a4c88be1b52ba2973f4e530
Billard <Mark.Billard@dfo-mpo.gc.ca>
2022-11-24 16:19:18 -0400

    added a fix for the 120315 licence issue in 2017 2018

--- MARFIS_GASPEREAU_unfinished.R
+++ MARFIS_GASPEREAU_unfinished.R
@@ -1,4 +1,5 @@
 catch.g<-catch[catch$RIVERNAME_CLEANED=="GASPEREAU",]
+catch.g<-catch.g[complete.cases(catch.g[,'LICENCE_ID']),] 
 
 ##from combing through logbooks
 catch.g$MEASUREMENT_UNIT[catch.g$YEAR==2019 & catch.g$LICENCE_ID==120146]<-"KILOGRAMS"
@@ -10,4 +11,12 @@ catch.g<-convert.KGS(catch.g)
 sum(catch.g$KGS[catch.g$YEAR==2019])
 
 catch.sub<-catch.g[catch.g$GEAR_DESCRIPTION!="SQUARE NET",]
-catch.ind<-catch.g[catch.g$LICENCE_ID==120124,]
+catch.ind<-catch.g[catch.g$LICENCE_ID==120315,]
+
+daykey<-c(4,5,9,12,16,19,22,23,25,26,29)
+
+catch.ind17<-subset(catch.ind,DAY %in% daykey & YEAR==2018)
+
+x<-sum(catch.ind17$FV_WEIGHT)+200+100+250+250
+
+x1=aggregate(catch.ind$KGS, by=list(YEAR=catch.ind$YEAR),FUN=sum)
