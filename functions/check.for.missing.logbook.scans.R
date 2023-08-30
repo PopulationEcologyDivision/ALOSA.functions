8ce96b7b83986252051840de79ea35b51463223c
Billard <Mark.Billard@dfo-mpo.gc.ca>
2023-08-30 12:28:44 -0300

    added check for missing logbook scans function

---
+++ functions/check.for.missing.logbook.scans.R
@@ -0,0 +1,61 @@
+# A function to output missing logs or unentered data from MARFIS in a year,
+# based on file names containing licence ID in a specified directory, and the
+# catch data frame in MARFIS
+#
+#TODO!!
+#read in csv of confirmed DNFs to filter out scanned logs with out catches,
+#since those don't have line entries in MARFIS
+#add DNF.file arguement to function
+#
+# Requires input of:
+# Catch dataframe from MARFIS_all in one.R
+#
+# This function is used by:
+#     - 
+#
+# This function uses:
+#     - 
+#
+# Example function call
+# missing.scans.2019<-check.for.missing.logbook.scans(directory="R:/Shared/BillardM/2019 FFLR scans Gaspereau/",
+#                                                     year=2019,
+#                                                     catch=catch,
+#                                                     write=F)
+
+
+
+check.for.missing.logbook.scans<-function(directory,year,catch,DNF.file,write=F)
+{
+  x<-list.files(directory,recursive=T)
+  log.scans<-gsub(".*/([0-9]{6}).*","\\1",x)
+  log.scans<-as.integer(log.scans) #might give warnings, thats fine
+  MARFIS.licences<-as.integer(unique(catch$LICENCE_ID[catch$YEAR==year]))
+  
+  ##I want the licence numbers in MARFIS NOT in scans
+  #these are data entries that we do not have a physical scan of
+  print(paste("Logs with catch in MARFIS that weren't scanned for",year))
+  print(setdiff(MARFIS.licences,log.scans))
+  if(write==T)
+  {write.csv(setdiff(MARFIS.licences,log.scans),file=paste("Unscanned logs",year),row.names=F)}
+  
+  ##doing the opposite, log scans that are not in MARFIS, should give us all DNF
+  ##logs, and also logs that were received but not entered (unlikely)
+  ##checking this vector against the confirmed DNF file should give 0
+  dnf<-read.csv("")
+  dnf<-dnf[dnf$YEAR==year,]
+  dnf.sub<-dnf$LICENCE_ID[dnf$NIL_REPORT=="Y" & dnf$NIL_CONFIRMED=="Y"]
+  scan.no.catch<-setdiff(log.scans,MARFIS.licences)
+  
+  print(paste("Scanned logs with no catch in MARFIS exclduing DNFs",year))
+  print(setdiff(log.scans,MARFIS.licences))
+  if(write==T)
+  {write.csv(setdiff(log.scans,MARFIS.licences),file=paste("Unentered logs",year),row.names=F)}
+}
+# 
+# missing.scans.2020<-check.for.missing.logbook.scans(directory="R:/Science/Population Ecology Division/DFD/Alosa/Freshwater Fishing Logbooks/Logbook Scans/2020_FFLR_Scans_Gaspereau/",
+#                         year=2020,
+#                         catch=catch)
+# 
+# missing.scans.2019<-check.for.missing.logbook.scans(directory="R:/Shared/BillardM/2019 FFLR scans Gaspereau/",
+#                                             year=2019,
+#                                             catch=catch)
