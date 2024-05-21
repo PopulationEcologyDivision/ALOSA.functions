c795e8397c375e66b4bc6cbb2bd785737009ea67
GRAY <LOGAN.GRAY@dfo-mpo.gc.ca>
2024-05-21 14:10:58 -0300

    lol

--- Assessment_Script_Tusket_Powerhouse_2024.R
+++ Assessment_Script_Tusket_Powerhouse_2024.R
@@ -1,11 +1,6 @@
 # In season ####
 setwd("R:/Science/Population Ecology Division/DFD/Alosa/Locations/Tusket River/Tusket 2024")
 
-# Make sure to save the xlsx sheet that we all use in the sharepoint to a csv.
-# It is located in: OneDrive - DFO-MPO\Alosa\Counts\2024.
-# I cannot figure out how to access the file path of stuff on sharepoint, so this
-# is currently the easiest way I know to update the data
-
 y <- onespecies.river.escapement(
   "Powerhouse 2024 count data.csv",
   fixtime = T,
@@ -21,8 +16,6 @@ n <- dim(y)[1]
 
 # This takes off the latest day, helpful for when it is incomplete.
 y <- y[1:n-1, ]
-
-#y$dayofyear<-as.numeric(as.character(y$dayofyear))
 
 print(paste0("Total escapement as of ", y$mon[n], "-", y$day[n], " is ", sum(y$total), sep = ""))
 
