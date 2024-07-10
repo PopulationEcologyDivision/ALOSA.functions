98118a6fb9b6dd7fd4da456337c88ec8c0dd646b
GRAY <LOGAN.GRAY@dfo-mpo.gc.ca>
2024-07-10 11:33:54 -0300

    moving scripts into single script for powerhouse and tusket

--- Assessment_Script_Tusket_2024.R
+++ Assessment_Script_Tusket_2024.R
@@ -59,3 +59,132 @@ n <- dim(x)[1]
 print(paste0("Total escapement as of ", x$mon[n], "-", x$day[n], " is ", sum(x$total), sep = ""))
 
 write.csv(x, file = "inseasonsummary.csv", row.names = F)
+
+# Email plot script
+
+library(tidyverse)
+
+# Load in the powerhouse data too so we can combine them later
+#source("C:/Users/graylo/Documents/GitHub/Rscripts/email_estimated_gaspereau_counts_tusket_powerhouse.R")
+
+# Calculate powerhouse escapement
+# In season ####
+setwd("R:/Science/Population Ecology Division/DFD/Alosa/Locations/Tusket River/Tusket 2024")
+
+y <- onespecies.river.escapement(
+  "Powerhouse 2024 count data.csv",
+  fixtime = T,
+  downstream.migration = F,
+  database = F,
+  2024,
+  2,
+  channel
+)
+
+y <- round(y)
+n <- dim(y)[1]
+
+# This takes off the latest day, helpful for when it is incomplete.
+#y <- y[1:n-1, ]
+
+print(paste0("Total escapement as of ", y$mon[n], "-", y$day[n], " is ", sum(y$total), sep = ""))
+
+write.csv(y, file = "powerhouse_inseasonsummary.csv", row.names = F)
+
+# Calculate count estimates, produces "x" which has count, date, etc
+#source("~/git/ALOSA.functions/Assessment_Script_Tusket_2024.R")
+
+# Write the counts to a csv
+# I couldn't figure out a way to access the data when it was just as "x" in R
+# so this is my roundabout way of doing things for now
+write_csv(x, "R:/Science/Population Ecology Division/DFD/Alosa/Locations/Tusket River/Tusket 2024/2024_counts.csv")
+
+# Get total count and represent it as a number with commas at the thousands places
+# for easier reading on the plot
+library(scales)
+total_count <- signif(sum(x$total), digits = 3)
+total_count_comma <- label_comma()(total_count)
+
+# Load old count data from Lake Vaughan for comparison with this year's and then
+# add the data from this year to this data set
+vaughan_wide <- read_csv("R:/Science/Population Ecology Division/DFD/Alosa/Locations/Tusket River/count_data_tusket_multi_year_wide.csv")
+count_2024 <- read_csv("R:/Science/Population Ecology Division/DFD/Alosa/Locations/Tusket River/Tusket 2024/2024_counts.csv")
+count_2024 <- count_2024 %>% select(mon, day, total) %>% rename(month = mon)
+vaughan_wide <- left_join(vaughan_wide, count_2024, by = c("month", "day"))
+vaughan_wide <- vaughan_wide %>% rename("2024" = total)
+
+# convert this wide data set to a long format for plotting
+vaughan_long <- vaughan_wide %>%
+  pivot_longer(
+    cols = "2014":"2024",
+    names_to = "year",
+    values_to = "count"
+  )
+
+# Make the dates for all observations, even those prior to 2024, as 2024.
+# This is on purpose to make them overlap when they plot. The actual year
+# of the observation is retained in the "year" column
+vaughan_long$date <- ymd(paste("2024", vaughan_long$month, vaughan_long$day, sep = "-"))
+
+# Add location to the data-frame so we can compare to the Powerhouse site
+vaughan_long$location <- "Lake Vaughan"
+
+# Extract the last full day for which data are available
+last_date <- make_datetime(year = 2024, month = tail(x$mon, 1), day = tail(x$day, 1))
+
+# Combine data-frames to show difference between the sites. We need to rename
+# some of the columns because they are labelled differently and drop a column
+# that is no longer useful
+powerhouse <- powerhouse %>% rename(month = mon, count = total)
+powerhouse <- powerhouse %>% select(-dayofyear)
+combined <- bind_rows(vaughan_long, powerhouse, .id = "source")
+
+# We only plot the observations since 2019 so as not to overwhelm the viewer
+# with too much on a single plot
+combined %>%
+  filter(year >= 2019) %>%
+  mutate(count = if_else(is.na(count) & date < "2024-04-20", 0, count)) %>% 
+  group_by(year) %>% 
+  ggplot(aes(date, count)) +
+  geom_path(data = . %>% filter(year != 2024), aes(colour = year), alpha = 0.25, linewidth = 1) +
+  geom_path(data = . %>% filter(year == 2024), aes(colour = year, linetype = location), alpha = 0.9, linewidth = 2) +
+  annotate(
+    "text",
+    x = as.Date("2024-04-23"),
+    y = 1.33e5,
+    label = paste("Total for Lake Vaughan (solid) as of", last_date, "is", total_count_comma),
+    size = 4.5
+  ) +
+  annotate(
+    "text",
+    x = as.Date("2024-04-23"),
+    y = 1.27e5,
+    label = paste("Total for Powerhouse (dashed) as of", pwr_last_date, "is", pwr_total_count_comma),
+    size = 4.5
+  ) +
+  theme_bw() +
+  labs(
+    title = "Estimated escapement for gaspereau on the Tusket River",
+    x = "Date",
+    y = "gaspereau / day",
+    colour = "Year",
+    linetype = "Location"
+  ) +
+  scale_x_date(date_labels = "%b %d", date_breaks = "1 week") +
+  scale_y_continuous(
+    labels = scales::comma,
+    n.breaks = 8) +
+  theme(
+    panel.grid.minor = element_blank(),
+    axis.title = element_text(face = "bold"),
+    axis.title.x = element_blank()
+  )
+
+combined %>%
+  filter(year == 2024) %>%
+  group_by(date) %>%
+  filter(all(c("Lake Vaughan", "Powerhouse") %in% location)) %>%
+  mutate(ratio = count[location == "Powerhouse"] / count[location == "Lake Vaughan"]) %>%
+  ggplot(aes(date, ratio)) +
+  geom_path(alpha = 0.75, linewidth = 1)+
+  scale_x_date(date_labels = "%b %d", date_breaks = "1 week")
