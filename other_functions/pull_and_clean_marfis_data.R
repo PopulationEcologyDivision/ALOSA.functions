bb965331b72477128ac2c15682244b55bc72d546
logan-gray-dfo <logan.gray@dfo-mpo.gc.ca>
2025-07-10 10:43:31 -0300

    I made a separate script to run when you want to do a fresh pull and clean of the MARFIS data.

---
+++ other_functions/pull_and_clean_marfis_data.R
@@ -0,0 +1,78 @@
+# Library
+library(ROracle)
+
+# This is where we keep our MARFIS pulls
+file_path <- "R:/Science/Population Ecology Division/DFD/Alosa/MARFISSCI/Old pulls/marfis_pull.Rdata"
+
+# Check if the MARFIS pull exists
+if (file.exists(file_path)) {
+  
+  # Get the last modification date of the MARFIS pull
+  file_date <- file.info(file_path)$mtime
+  
+  # Get the current date
+  current_date <- Sys.Date()
+  
+  # Prompt the user whether or not to update the MARFIS pull
+  response <- readline(prompt = paste0("The last MARFIS pull is from ", as.Date(file.info(file_path)$mtime), ". Would you like to update it? (y / n): "))
+  
+  # Process user response
+  if (tolower(response) == "y" | tolower(response) == "yes") {
+    
+    message("Updating the MARFIS pull")
+    
+    # Connect to MARFIS db and pull
+    oracle.password <- "zpdUG7!W"
+    oracle.username <- "BILLARDM"
+    
+    channel <- dbConnect(
+      DBI::dbDriver("Oracle"),
+      username = oracle.username,
+      password = oracle.password,
+      dbname = "PTRAN",
+      believeNRows = FALSE
+    )
+    
+    source("~/git/ALOSA.functions/MARFIS_all in one.R")
+    save(catch, didnotfish, licencerenewals, file = file_path)
+    
+  } else {
+    message(paste0("Using previous MARFIS pull from ", as.Date(file.info(file_path)$mtime)))
+  }
+  
+} else {
+  message("MARFIS pull not found")
+  message("Pulling data from MARFIS ...")
+  
+  # Connect to MARFIS db and pull
+  oracle.password <- "zpdUG7!W"
+  oracle.username <- "BILLARDM"
+  
+  channel <- dbConnect(
+    DBI::dbDriver("Oracle"),
+    username = oracle.username,
+    password = oracle.password,
+    dbname = "PTRAN",
+    believeNRows = FALSE
+  )
+  source("~/git/ALOSA.functions/MARFIS_all in one.R")
+  save(catch, didnotfish, licencerenewals, file = file_path)
+  message("Updated MARFIS pull saved")
+  
+}
+
+# Load in the data
+load(file_path)
+message("MARFIS data loaded")
+
+# Clean the data
+message("Cleaning MARFIS data ...")
+source("R:/Science/Population Ecology Division/DFD/Alosa/MARFISSCI/MARFIS.error.cleaner.R")
+message("MARFIS data cleaned")
+
+#Convert any landings listed as pounds to kilograms
+message("Converting catch units to kilograms ...")
+catch <- convert.KGS(catch)
+catch <- subset(catch, select = -FV_WEIGHT)
+catch <- catch |> rename(FV_WEIGHT = KGS)
+message("Units converted")
