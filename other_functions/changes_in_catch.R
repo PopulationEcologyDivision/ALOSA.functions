b7d784ec3968a8e42a98b4fc9929edce5cdbd4b9
logan-gray-dfo <logan.gray@dfo-mpo.gc.ca>
2025-07-22 09:24:41 -0300

    Returns a df that shows percent changes in landings

--- other_functions/changes_in_catch.R
+++ other_functions/changes_in_catch.R
@@ -6,13 +6,25 @@
 # data = the catch dataframe from MARFIS
 # max_percent = how much of a percentage increase you are interested in
 # max_diff = the max difference you want to see between years
+# round_digits = how many decimal points you want in the rounded values
+# verbose = tells you how many rows you filtered out
 
 changes_in_catch <- function(
     data,
     max_percent = NULL,
-    max_diff = NULL
+    max_diff = NULL,
+    round_digits = 0,
+    verbose = FALSE
     ) {
   
+  # Check for missing columns
+  required_cols <- c("LICENCE_ID", "YEAR", "FV_WEIGHT", "RIVERNAME_CLEANED")
+  missing_cols <- setdiff(required_cols, names(data))
+  if (length(missing_cols > 0)) {
+    stop("You are missing the required columns: ", paste(missing_cols, collapse = ", "))
+  }
+  
+  # Calculate summaries
   data |> 
     group_by(LICENCE_ID, YEAR) |> 
     summarise(
@@ -23,13 +35,14 @@ changes_in_catch <- function(
     group_by(LICENCE_ID) |> 
     mutate(
       PREV_WEIGHT = lag(TOTAL_WEIGHT),
-      DIFF_WEIGHT = round(TOTAL_WEIGHT - PREV_WEIGHT),
-      PERCENT_CHANGE = round((TOTAL_WEIGHT - PREV_WEIGHT) / PREV_WEIGHT * 100),
-      TOTAL_WEIGHT = round(TOTAL_WEIGHT),
-      PREV_WEIGHT = round(PREV_WEIGHT)
+      DIFF_WEIGHT = round(TOTAL_WEIGHT - PREV_WEIGHT, round_digits),
+      PERCENT_CHANGE = round((TOTAL_WEIGHT - PREV_WEIGHT) / PREV_WEIGHT * 100, round_digits),
+      TOTAL_WEIGHT = round(TOTAL_WEIGHT, round_digits),
+      PREV_WEIGHT = round(PREV_WEIGHT, round_digits)
     ) |> 
-    select(YEAR, RIVERNAME_CLEANED, LICENCE_ID, TOTAL_WEIGHT, PREV_WEIGHT, DIFF_WEIGHT, PERCENT_CHANGE) -> catch_changes
+    select(YEAR, RIVERNAME_CLEANED, LICENCE_ID, TOTAL_WEIGHT, PREV_WEIGHT, DIFF_WEIGHT, PERCENT_CHANGE)
   
+  # Check for max percent
   if (is.null(max_percent)) {
     return(catch_changes)
   } else {
@@ -37,12 +50,15 @@ changes_in_catch <- function(
       filter(abs(PERCENT_CHANGE) <= max_percent) -> catch_changes
   }
   
+  # Check for max difference
   if (is.null(max_diff)) {
     return(catch_changes)
   } else {
     catch_changes |> 
       filter(abs(DIFF_WEIGHT) <= max_diff) -> catch_changes
   }
+  
+  if (verbose) { message("Returning ", nrow(catch_changes), " rows after filtering.") }
   
   return(catch_changes)
   
