3ff27d9b2cc55dd24d522a9a1811ccc7294c1a4e
Billard <Mark.Billard@dfo-mpo.gc.ca>
2026-05-13 15:18:52 -0300

    addde 2026 GRA assessment rmd. updated escapement functions to run off of an R object in addition to a filename. other misc items

--- functions/twospecies.river.escapement.R
+++ functions/twospecies.river.escapement.R
@@ -5,7 +5,7 @@
 # This should be added in in the future.
 #
 # Inputs:
-#        - filename: filename where escapement counts are
+#        - count.data: R dataframe of counts, OR filename where escapement counts are
 #        - fixtime=F: default is F but use T if the minutes/seconds columns are present
 #        - downstream.migration=F: if true sets all up-down=Total that are negative to 0
 #        - database=T: pull count data from database instead of local file (prefered method)
@@ -26,7 +26,7 @@
 #     - dplyr package
 
 
-twospecies.river.escapement<-function(filename,
+twospecies.river.escapement<-function(count.data,
                                       fixtime=T,
                                       downstream.migration=F,
                                       database=T,
@@ -50,8 +50,8 @@ twospecies.river.escapement<-function(filename,
     names(count.data)[9]<-"seconds"
   }
   if(database==F){
-    count.data=read.csv(filename,header=T,
-                        stringsAsFactors = F)
+    #read in count data if only given a filename
+    if(is.character(count.data)==T){count.data = read.csv(count.data, header = T, stringsAsFactors = F)}
     
     if(fixtime==F){
       goodnames=(c("year","mon","day","time","strata","count.upstream",
